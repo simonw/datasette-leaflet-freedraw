b20c3e7b8db0c414a32e92fe79666c39698a07e6
Simon Willison <swillison@gmail.com>
2021-12-17 12:50:03 -0800

    New test that uses SpatiaLite

--- tests/test_leaflet_freedraw.py
+++ tests/test_leaflet_freedraw.py
@@ -1,7 +1,10 @@
 from datasette.app import Datasette
+from datasette.utils.sqlite import sqlite3
 from datasette.utils.asgi import Request
+from datasette.utils import find_spatialite
 from datasette_leaflet_freedraw import extra_body_script, extra_js_urls
 import pytest
+import secrets
 
 
 @pytest.mark.asyncio
@@ -57,3 +60,78 @@ async def test_extra_body_script():
         "    current_geojson: null\n"
         "};"
     )
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "table,expect_map,expect_spatial_index",
+    (
+        (
+            "places_no_spatialite",
+            False,
+            False,
+        ),
+        (
+            "places_spatialite",
+            True,
+            False,
+        ),
+        (
+            "places_spatialite_spatial_index",
+            True,
+            True,
+        ),
+    ),
+)
+async def test_show_for_table_only_if_spatialite(
+    table, expect_map, expect_spatial_index
+):
+    dbname = "geodb{}".format(secrets.token_hex(4))
+    datasette = Datasette(memory=True, files=[], sqlite_extensions=["spatialite"])
+    db = datasette.add_memory_database(dbname)
+
+    def setup(db):
+        db.enable_load_extension(True)
+        db.execute("SELECT load_extension(?)", [find_spatialite()])
+        db.execute("SELECT InitSpatialMetadata(1)")
+        db.execute(
+            "CREATE TABLE places_no_spatialite (id integer primary key, name text)"
+        )
+        db.execute("CREATE TABLE places_spatialite (id integer primary key, name text)")
+        db.execute(
+            "SELECT AddGeometryColumn('places_spatialite', 'geometry', 4326, 'POINT', 'XY');"
+        )
+        db.execute(
+            "CREATE TABLE places_spatialite_spatial_index (id integer primary key, name text)"
+        )
+        db.execute(
+            "SELECT AddGeometryColumn('places_spatialite_spatial_index', 'geometry', 4326, 'POINT', 'XY');"
+        )
+        db.execute(
+            "SELECT CreateSpatialIndex('places_spatialite_spatial_index', 'geometry');"
+        )
+
+    await db.execute_write_fn(setup, block=True)
+
+    html = (await datasette.client.get("/{}/{}".format(dbname, table))).text
+    if expect_map:
+        assert "show_for_table: true," in html
+    else:
+        assert "show_for_table: false," in html
+
+    if expect_spatial_index:
+        # Feed it some GeoJSON and see if the SQL changes to the right value
+        html2 = (
+            await datasette.client.get(
+                "/{}/{}?_freedraw={}".format(dbname, table, "{}")
+            )
+        ).text
+        # Extract the SQL from the "View and edit SQL" link
+        sql = html2.split('<p><a class="not-underlined" title="')[1].split('" href')[0]
+        assert sql == (
+            "select id, name, geometry from places_spatialite_spatial_index "
+            "where Intersects(GeomFromGeoJSON(:freedraw), [geometry]) = 1 "
+            "and [places_spatialite_spatial_index].rowid in "
+            "(select rowid from SpatialIndex where f_table_name = :freedraw_table\n"
+            "and search_frame = GeomFromGeoJSON(:freedraw)) order by id limit 101"
+        )
